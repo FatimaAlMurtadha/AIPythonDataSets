8caf5d59a89a146a37e15fce9c6b19cee0354330
FatimaAlmurtadha <sukaina77150@gmail.com>
2026-04-29 14:42:14 +0200

    mutable and immutable

---
+++ python-modules/classes/my_class.py
@@ -0,0 +1,7 @@
+class MyClass:
+    def __init__(self, value):
+        self.value = value
+
+    def increment(self, another_class):
+        self.value += 1
+        another_class +=2
--- python-modules/main.py
+++ python-modules/main.py
@@ -1,7 +1,43 @@
 from directory.module import my_awesome_function
 from flowers.roses import list_roses
+from classes.my_class import MyClass
 
 my_roses = list_roses()
 print()
 
 my_awesome_function()
+
+c1 = MyClass(1)
+c2 = MyClass(2)
+
+print(c1.value)
+
+
+print(c1.value)
+print(c2.value)
+
+def add1(items):
+    items.append("!")
+
+xs = ["hello"]
+add1(xs)
+print(xs)
+
+def add2(s):  
+  s += "!"
+  return s
+
+greeting = "hello"
+add2(greeting)
+print(greeting)
+
+greetings2 = add2(greeting)
+print(greetings2)
+
+def append_to(item, target=[]):
+    target.append(item)
+    return target
+
+print(append_to(1)) # [1]
+print(append_to(2)) # [1, 2] - the list is shared between calls, which can lead to unexpected behavior
+
